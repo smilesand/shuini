d689a4985efc5d1bf747734e7d44beaec81018ed
smilesand <847666418@qq.com>
2026-07-14 12:05:20 +0800

    修改文发布文件

--- backend/core/config.py
+++ backend/core/config.py
@@ -8,15 +8,7 @@ from pathlib import Path
 
 
 DEFAULT_CORS_ORIGINS = (
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "http://localhost:5174",
-    "http://127.0.0.1:5174",
-    "http://localhost:5175",
-    "http://127.0.0.1:5175",
-    "http://localhost:5176",
-    "http://127.0.0.1:5176",
-    "http://1.117.73.146",
+    ".*",
 )
 
 
